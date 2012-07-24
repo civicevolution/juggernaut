8201bcc34f896668dc8f77256d6215ba1a8a32c2
Brian Sullivan <brian@civicevolution.org>
2012-07-24 15:04:58 -0700

    Original juggernaut from mcaman github with CE updates

--- server.js
+++ server.js
@@ -17,5 +17,30 @@ if (argv.h || argv.help) {
   return util.puts(help);
 }
 
+if( require('fs').existsSync("/ce_development") ){
+	if(argv.log){
+		console.log('write juggernaut log to /ce_development/log/juggernaut.log' );
+		var stdo = require('fs').createWriteStream('/ce_development/log/juggernaut.log', {flags: 'a'});
+		process.stdout.write = (function(write) {
+			return function(string, encoding, fd) {
+				stdo.write(string);
+			}
+		})(process.stdout.write)
+	}
+	
+	//console.log('write juggernaut process pid ' + process.pid);
+	var fs = require('fs');
+	fs.writeFile("/ce_development/log/juggernaut.pid", process.pid, function(err) {
+	    if(err) {
+	        console.log(err);
+	    } else {
+	        //console.log("The file was saved!");
+	    }
+	});
+	
+}
+
+process.title = 'CE'; // doesn't work on OS X
+
 Juggernaut = require("./index");
 Juggernaut.listen(argv.port);
